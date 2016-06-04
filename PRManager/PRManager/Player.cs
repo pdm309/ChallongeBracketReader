aa81528a75ed3ba31714eace7ae2517583b7847e
pdm309 <pdm309@nyu.edu>
2016-06-03 19:04:20 -0700

    committing intro of PRManager

---
+++ PRManager/PRManager/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRManager
+{
+    public class Player
+    {
+        public int bounty;
+        public string name;
+        public string tag;
+        public int rank;
+        public string character;
+        
+        public Player()
+        {
+            bounty = 0;
+            name = null;
+            tag = null;
+            rank = 0;
+            character = null;
+        }
+        public Player(int bounty, string name, string tag, int rank, string character)
+        {
+            this.bounty = bounty;
+            this.name = name;
+            this.tag = tag;
+            this.rank = rank;
+            this.character = character;
+        }
+    }
+}
---
+++ PRManager/PRManager/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRManager
+{
+    class Program
+    {
+        //placement or win/loss or both
+        //every recording adds to winner and subtracts from loser
+        //  add the bounty of who you beat, subtract from them what yours is
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a player tag/name");
+            string nameOrTag = Console.ReadLine();
+            string line;
+            bool nameOrTagFound = false;
+            System.IO.StreamReader file = new System.IO.StreamReader("smashcharacters.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains(nameOrTag))
+                {
+                    nameOrTagFound = true;
+                }
+            }
+
+            if (nameOrTagFound == true)
+            {
+
+            }
+            else
+            {
+
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void updatePlayer(Player player)
+        {
+
+        }
+
+
+        static void addPlayer(Player player)
+        {
+
+        }
+
+        static void addResult(Player playerWon, Player playerLost)
+        {
+
+        }
+    }
+}
