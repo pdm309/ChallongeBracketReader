56a14b497517d3074baee2727ff0bacf2411bb58
Paul Merritt <pdm309@nyu.edu>
2016-12-01 20:32:50 -0500

    Search for Player wins/losses added

---
+++ ChallongeBracketReader/ChallongePortal.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+namespace ChallongeBracketReader
+{
+    class ChallongePortal
+    {
+        private readonly RestClient client;
+
+        public string ApiKey { get; private set; }
+        public string Subdomain { get; private set; }
+
+        public ChallongePortal(string apiKey) : this(apiKey, null) { }
+
+        public ChallongePortal(string apiKey, string subdomain)
+        {
+            client = new RestClient(@"https://api.challonge.com/v1/");
+            ApiKey = apiKey;
+            Subdomain = subdomain;
+        }
+
+        private void throwOnError(IRestResponse response)
+        {
+            if (response.ResponseStatus != ResponseStatus.Completed || response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                
+                throw new ChallongeApiException(response);
+            }
+        }
+
+        public IEnumerable<Tournament> GetTournaments()
+        {
+            var request = new RestRequest("tournaments.xml", Method.GET);
+            request.AddParameter("api_key", ApiKey);
+            if (!string.IsNullOrWhiteSpace(Subdomain)) request.AddParameter("subdomain", Subdomain);
+
+            var response = client.Execute<List<Tournament>>(request);
+            throwOnError(response);
+
+            return response.Data;
+        }
+
+        public Tournament ShowTournament(int tournamentId)
+        {
+            var request = new RestRequest(string.Format("tournaments/{0}.xml", tournamentId), Method.GET);
+            request.AddParameter("api_key", ApiKey);
+
+            var response = client.Execute<Tournament>(request);
+            throwOnError(response);
+
+            return response.Data;
+        }
+
+        public IEnumerable<Match> GetMatches(int tournamentId)
+        {
+            var request = new RestRequest(string.Format("tournaments/{0}/matches.xml", tournamentId), Method.GET);
+            request.AddParameter("api_key", ApiKey);
+
+            var response = client.Execute<List<Match>>(request);
+            throwOnError(response);
+
+            return response.Data;
+        }
+
+        public IEnumerable<Participant> GetParticipants(int tournamentId)
+        {
+            var request = new RestRequest(string.Format("tournaments/{0}/participants.xml", tournamentId), Method.GET);
+            request.AddParameter("api_key", ApiKey);
+
+            var response = client.Execute<List<Participant>>(request);
+            throwOnError(response);
+
+            return response.Data;
+        }
+
+        public void SetParticipantMisc(int tournamentId, int participantId, string misc)
+        {
+            var request = new RestRequest(string.Format("tournaments/{0}/participants/{1}.xml", tournamentId, participantId), Method.PUT);
+            request.AddParameter("api_key", ApiKey);
+            request.AddParameter("participant[misc]", misc);
+
+            var response = client.Execute(request);
+            throwOnError(response);
+        }
+
+        public void ReportMatchWinner(int tournamentId, int matchId, int winnerId, int[] scores)
+        {
+            var request = new RestRequest(string.Format("tournaments/{0}/matches/{1}.xml", tournamentId, matchId), Method.GET);
+            request.AddParameter("api_key", ApiKey);
+            request.AddParameter("tournament", tournamentId);
+            request.AddParameter("match", matchId);
+
+            //scores = new SetScore[] { new SetScore { Player1Score = 0, Player2Score = 0 } };
+
+
+            /*Trying to access scores*/
+            //client.DownloadData(requestScores).SaveAs("scores");
+
+
+            String score = "scores_csv";
+            var requestScores = new RestRequest(string.Format("tournaments/{0}/matches/{1}/attachments/{2}.{json|xml}", tournamentId, matchId, score), Method.GET);
+            requestScores.AddParameter("api_key", ApiKey);
+            requestScores.AddParameter("tournament", tournamentId);
+            requestScores.AddParameter("match", matchId);
+            requestScores.AddParameter("attachment", score);
+
+            //requestScores.AddParameter("match[scores_csv]", scores.Select(ss => ss.ToString()).Aggregate((one, two) => string.Format("{0},{1}", one, two)));
+            var responseScores = client.Execute(requestScores);
+            throwOnError(responseScores);
+            /*Trying to access scores*/
+
+
+            var response = client.Execute(request);
+            throwOnError(response);
+
+        }
+
+        public void EndTournament(int tournamentId)
+        {
+            var request = new RestRequest(string.Format("tournaments/{0}/finalize.xml", tournamentId), Method.POST);
+            request.AddParameter("api_key", ApiKey);
+
+            var response = client.Execute<List<Participant>>(request);
+            throwOnError(response);
+        }
+    }
+}
--- ChallongeBracketReader/Program.cs
+++ ChallongeBracketReader/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-
+using RestSharp;
 namespace ChallongeBracketReader
 {
     class Product
@@ -19,19 +19,106 @@ namespace ChallongeBracketReader
     {
         static void Main(string[] args)
         {
-
-
+            //Console.WriteLine("Write the challonge bracket url in full: \n>");
+            //string url = Console.ReadLine();
             //check url for errors such as not having challonge or .com or maybe even http(s) and www
-            Console.WriteLine("Enter challonge tournament url: \n");
-            string url = Console.ReadLine();
-            if (Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+            Console.WriteLine("Enter challonge tournament apikey: \n");
+            string api = Console.ReadLine();
+
+            ChallongePortal challongePortal = new ChallongePortal(api);
+            IEnumerable<Tournament> tournaments = challongePortal.GetTournaments();
+            foreach (Tournament tournament in tournaments)
             {
-                RunAsync(url).Wait();
-                Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine(tournament.Name);
             }
+            Console.WriteLine("Which tournament would you like to see match reports of?");
+            string tourneyChoice = Console.ReadLine();
+
+            Console.WriteLine("Is there a specific player you'd like results of? Please write a name/tag or NO");
+            string playerChoice = Console.ReadLine();
+
+            Tournament tourney = ValidateTournament(tourneyChoice, tournaments);
+            IEnumerable<Match> matches;
+            if (tourney!=null)
+            {
+                matches = challongePortal.GetMatches(tourney.Id);
+                IEnumerable<Participant> participants = challongePortal.GetParticipants(tourney.Id);
+                foreach (Match match in matches)
+                {
+                    int[] score = new int[2];
+
+                    string player1 = "";
+                    string player2 = "";
+                    Participant part1 = new Participant();
+                    Participant part2 = new Participant();
+                    bool player1Won = false;
+                    foreach (Participant participant in participants)
+                    {
+                        if (participant.Id == match.Player1Id)
+                        {
+                            player1 = participant.NameOrUsername;
+                            part1 = participant;
+                            if (match.WinnerId == match.Player1Id)
+                            {
+                                player1Won = true;
+                            }
+                        }
+                        else if (participant.Id == match.Player2Id)
+                        {
+                            part2 = participant;
+                            player2 = participant.NameOrUsername;
+//                            if (player2 == null || player2 == "")
+//                            {
+//                                player2 = participant.ChallongeUsername;
+//                            }
+
+                        }
+                    }
+
+                    if (player1Won)
+                    {
+                        challongePortal.ReportMatchWinner(tourney.Id, match.Id, part1.Id, score);
+                        if (player1.Contains(playerChoice) || player2.Contains(playerChoice) || playerChoice == "" || playerChoice.ToUpper() == "NO")
+                        {
+                            Console.WriteLine("\nRound " + match.Round + ": " + player1 + " defeated " + player2);// + " in a score " + score.ToString());
+                        }
+                    }
+                    else
+                    {
+                        challongePortal.ReportMatchWinner(tourney.Id, match.Id, part2.Id, score);
+                        if (player1.Contains(playerChoice) || player2.Contains(playerChoice) || playerChoice == "" || playerChoice.ToUpper() == "NO")
+                        {
+                            Console.WriteLine("\nRound " + match.Round + ": " + player2 + " defeated " + player1);
+                                // + " in a score " + score.ToString());
+                        }
+                    }
+                    
+                }
+                //Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("INVALID TOURNAMENT NAME");
+                Console.ReadLine();
+                return;
+            }
+            
             
         }
 
+        static Tournament ValidateTournament(string tourneyChoice, IEnumerable<Tournament> tournaments)
+        {
+            foreach (Tournament tournament in tournaments)
+            {
+                if (tourneyChoice == tournament.Name)
+                {
+                    return tournament;
+                }
+            }
+            return null;
+        }
+        
         static async Task RunAsync(string url)
         {
             using (var client = new HttpClient())
